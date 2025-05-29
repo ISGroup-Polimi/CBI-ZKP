89e65d61e44bcd220e8e2af7acfcdb3965f4585d
Nicolas Carboni <nicolas.carboni@mail.polimi.it>
2025-05-29 10:56:19 +0200

    logrows = 20

--- main.py
+++ main.py
@@ -298,7 +298,7 @@ async def op_perform_query(file_path, operations, columns_to_remove):
         json.dump(data, f) # serialize the data dictionary to a JSON file
 
     #await generate_proof(output_dir, model_onnx_path, input_json_path, logrows=17)
-    await generate_proof(output_dir, model_onnx_path, input_json_path, logrows=15)
+    await generate_proof(output_dir, model_onnx_path, input_json_path, logrows=20)
 
     # Print and save the final tensor after applying the OLAP operations in human-readable format
     # Remove rows from final_tensor that are all zeros
