a6819fb31f6fa4bd7ce26106defea9a55a3e2562
Nicolas Carboni <nicolas.carboni@mail.polimi.it>
2025-09-18 16:49:59 +0200

    print products2

--- Shared/Dim_ID_Converter.py
+++ Shared/Dim_ID_Converter.py
@@ -67,6 +67,7 @@ print(products)
 
 # Return the list of product names from a list of product IDs
 def get_products_from_ids(product_ids):
+    print(products)
     result = []
     for pid in product_ids:
         key = str(pid)
