eeabbc3db9b4ccfdccff62aa9f0e65064fd0e1e6
Nicolas Carboni <nicolas.carboni@mail.polimi.it>
2025-09-23 19:14:51 +0200

    remove TS column

--- Org1/execute_query.py
+++ Org1/execute_query.py
@@ -36,6 +36,12 @@ async def op_execute_query(operations, columns_to_remove_idx, timestamp):
     df = pd.read_csv(file_path)
     # df = df[df["TS"] <= timestamp] # select rows with TS <= timestamp
 
+
+    # testing
+    if "TS" in df.columns:
+        df = df.drop(columns=["TS"])
+        print("Timestamp column dropped.")
+
     df.columns = df.columns.str.strip() # Remove leading and trailing whitespace from column names
     df = df.dropna() # Drop rows with NaN values
 
